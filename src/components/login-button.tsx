ba2cfeccce8a43f43bb9a4fb2002418d765bd6da
Manassarn Manoonchai <manassarn@gmail.com>
2022-08-12 23:29:31 +0700

    Add DaisyUI & mock blog posts

--- src/components/login-button.tsx
+++ src/components/login-button.tsx
@@ -3,18 +3,22 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function LoginButton() {
   const { data: session } = useSession()
 
-  if (session) {
+  if (session?.user) {
     return (
       <>
-        Signed in as {session?.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        {session.user.name}
+        <button className="btn btn-sm" onClick={() => signOut()}>
+          Sign out
+        </button>
       </>
     )
   }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button className="btn btn-sm" onClick={() => signIn()}>
+        Sign in
+      </button>
+      to start posting
     </>
   )
 }
